8010e44379a6eff3766acb3b975e74a0d971477b
Marcelo Braga <b3darcademy@outlook.com>
2022-05-11 05:16:11 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-// GithubIcon foi colocado na forma de uma tag porque o componente react vai virar uma sintaxe de tag.
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+
+
+function Listing() {
+    return(
+        <h1>Página de listagem</h1>
+    )
+}
+
+export default Listing;

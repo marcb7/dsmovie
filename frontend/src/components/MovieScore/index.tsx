300e8bcc332cacd02d27db550d1f242c630896c9
Marcelo Braga <b3darcademy@outlook.com>
2022-05-15 09:58:22 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,14 +1,18 @@
 import MovieStars from "components/MovieStars";
 import './styles.css';
 
-function MovieScore() {
-    const score = 3.5;
-    const count = 13;
+type Props = {
+    score: number;
+    count: number;
+}
+
+// Esse meu MovieScore recebe o score e o count e repassa o score para o MovieStars.
+function MovieScore({ score, count }: Props) {
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
